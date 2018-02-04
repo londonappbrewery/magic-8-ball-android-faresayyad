e457ec6d8f20c04718b4eddf14d78a75da719930
faresayyad <fares.explorer@gmail.com>
2018-02-04 22:52:42 +0200

    My Course work Gradle Plugin & Build Tool Update

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,12 @@
 package com.londonappbrewery.magiceightball;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay= findViewById(R.id.iv_eightBall);
+
+        final int[] ballArray=new int[]{
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        Button askBtn=findViewById(R.id.btnAsk);
+
+        askBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random numberGen =new Random();
+                int number =numberGen.nextInt(5); // which is from 0 to 4
+//                Log.d("Button", "Number is: "+ number);
+                ballDisplay.setImageResource(ballArray[number]);
+            }
+        });
     }
 }
